0d9cb94b43d1dd18fb3aff9e20fa0a3e457675cc
Sam Bulman <161944461+elemelon202@users.noreply.github.com>
2025-11-10 16:06:15 +0900

    added testing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,17 +12,20 @@ class GamesController < ApplicationController
     @letters = alphabet.sample(10).map {|l| l.upcase}
   end
 
-  def score
-    input = params[:word].chars
-    in_grid = input.all? { |char| params[:letters].count(char) >= input.count(char) }
+def score
+  word = params[:word].downcase
+  letters = params[:letters].downcase.chars.tally
+  input_chars = word.chars
 
-    if !api_send("https://dictionary.lewagon.com/#{input.join("")}")
-    @message = "Mate....I legit checked and that is not an english word"
-    elsif !in_grid
-    @message = "You entered #{input.join("").upcase}, but you've gotta use only the available letters"
-    else
-    @score = "YOU SCORE: #{input.length * 100}"
+  in_grid = input_chars.all? { |char| input_chars.count(char) <= letters[char].to_i }
+
+  if !api_send("https://dictionary.lewagon.com/#{word}")
+    @message = "Mate....I legit checked and that is not an English word"
+  elsif !in_grid
+    @message = "You entered #{word.upcase}, but you've gotta use only the available letters"
+  else
+    @score = "You scored: #{input_chars.length * 100}"
     @message = "Well done"
-    end
   end
+end
 end
